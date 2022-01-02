eb99ef473f58732645e907ae4ed2469cbddf0805
islomabduqodir <islomabduqodir@gmail.com>
2022-01-02 20:11:17 +0500

    second push

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(".teach").hide();  
+$(".teach1").show();  
+
+
+$(".first").click(function () { 
+    $(".num").removeClass("active");  
+    $(".first").addClass("active");  
+    $(".teach").hide();  
+    $(".teach1").show();  
+});
+$(".second").click(function () { 
+    $(".num").removeClass("active");  
+    $(".second").addClass("active");  
+    $(".teach").hide();  
+    $(".teach2").show();  
+
+});
+$(".third").click(function () { 
+    $(".num").removeClass("active");  
+    $(".third").addClass("active");  
+    $(".teach").hide();  
+    $(".teach3").show();  
+});
+$(".fourth").click(function () { 
+    $(".num").removeClass("active");  
+    $(".fourth").addClass("active");  
+    $(".teach").hide();  
+    $(".teach4").show();  
+});
+$(".fifth").click(function () { 
+    $(".num").removeClass("active");  
+    $(".fifth").addClass("active"); 
+    $(".teach").hide();  
+    $(".teach5").show();  
+});
